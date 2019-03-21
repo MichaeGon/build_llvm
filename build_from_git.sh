bd9eca1dc09e9b91a51ed41c17d39f2bc44d9831
MichaeGon <lodnix@gmail.com>
2019-03-21 09:10:53 +0900

    fix

--- build_from_git.sh
+++ build_from_git.sh
@@ -6,7 +6,7 @@ TYPE=Release
 PROJS="clang;clang-tools-extra;libcxx;libcxxabi;lldb;compiler-rt;lld;polly"
 
 rm -rf ${DIR}
-git pull origin master
+#git pull origin master
 mkdir ${DIR}
 cd ${DIR}
 cmake -G Ninja -DLLVM_EXPERIMENTAL_TARGETS_TO_BUILD=WebAssembly -DLLVM_TARGETS_TO_BUILD=all -DCMAKE_INSTALL_PREFIX=${PREFIX} -DLLVM_ENABLE_PROJECTS=${PROJS} -DCMAKE_BUILD_TYPE=${TYPE} ../llvm
